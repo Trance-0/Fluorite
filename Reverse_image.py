0d4aa154e8302a37c42f68521f22bd839aba7602
Trance <wuzheyuan.86@gmail.com>
2020-04-25 22:59:32 +0800

    2020.4.25

---
+++ Reverse_image.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+# Read an image file
+path = os.path.dirname(os.path.abspath(__file__))
+magic = path + '/' + 'Screen Shot 2020-04-23 at 22.19.34.png'
+image = Image.open(magic)
+x, y = image.size
+pim = image.load()
+# Add some color boundaries to modify an image array
+for height in range (x):
+    for width in range (y):
+        a=tuple((256-image.getpixel((height,width))[0],256-image.getpixel((height,width))[1],256-image.getpixel((height,width))[2]))
+        pim[height,width]= a
+# use pyplot to plot the image
+image.show()
