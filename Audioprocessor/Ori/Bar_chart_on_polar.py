08bf5363711ae6e08ed6459c3b1e97163f3470be
Trance <wuzheyuan.86@gmail.com>
2020-06-17 14:04:45 +0800

    2020.6.17

--- Audioprocessor/Ori/Bar_chart_on_polar.py
+++ Audioprocessor/Ori/Bar_chart_on_polar.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 # np.random.seed(19680823)
 
 # Compute pie slices
-N = 10
+N = 20
 theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
 # radii=8
 radii = 10 * np.random.rand(N)
 # width=1
 # width = np.pi / 4 * np.random.rand(N)
-width=np.pi/N*2
+width=2*np.pi/N
 
 colors = plt.cm.viridis(radii / 10.)
 
