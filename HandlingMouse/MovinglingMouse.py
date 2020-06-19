0d465ad98d9be55c8044c76d11c7eab4636c3f21
Trance <wuzheyuan.86@gmail.com>
2020-06-19 19:49:21 +0800

    2020.6.19

--- HandlingMouse/MovinglingMouse.py
+++ HandlingMouse/MovinglingMouse.py
@@ -13,30 +13,21 @@ def on_release(key):
     print('{0} have been released'.format(key))
     position=mouse.position
     if  key == keyboard.Key.up:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(0,-10)
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(0,-10)
     if key == keyboard.Key.down:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(0,10)
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(0,10)
     if key == keyboard.Key.left:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(-10,0)
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(-10,0)
     if key == keyboard.Key.right:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(10,0)
-    if key == keyboard.Key.f13:
-        print('********')
-        mouse.press(Button.left)
-        mouse.release(Button.left)
-    if key == keyboard.Key.f15:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.press(Button.right)
-        mouse.release(Button.right)
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(10,0)
     if key == keyboard.Key.esc:
         # Stop listener
         return False
