6474eb6fd4387f2e529529cb77aca1fdadb2a931
Trance <wuzheyuan.86@gmail.com>
2020-06-19 11:33:09 +0800

    2020.6.19

--- HandlingMouse/HandlingMouse.py
+++ HandlingMouse/HandlingMouse.py
@@ -1,19 +1,33 @@
 from pynput import keyboard
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener   
 from pynput.mouse import Button, Controller
-mouse = Controller()
+
+mouse = Controller() 
 def on_press(key):
     try:
-        position=mouse.position
         print('key {0} is pressed'.format(key.char))
-        if keyboard.read_key() == "p":
-            print('********')
-            mouse.move(position[0]+10,posision[1])
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
 def on_release(key):
     print('{0} have been released'.format(key))
+    position=mouse.position
+    if  key == keyboard.Key.up:
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(0,-10)
+    if key == keyboard.Key.down:
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(0,10)
+    if key == keyboard.Key.left:
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(-10,0)
+    if key == keyboard.Key.right:
+            print('********')
+            # mouse.move(position[0]+10,position[1])
+            mouse.move(10,0)
     if key == keyboard.Key.esc:
         # Stop listener
         return False
@@ -25,3 +39,4 @@ with keyboard.Listener( on_press=on_press,on_release=on_release) as listener:
     # if on_press           
 
 
+           
--- HandlingMouse/Ori/Example.py
+++ HandlingMouse/Ori/Example.py
@@ -2,11 +2,9 @@ from pynput import keyboard
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
 
 def on_release(key):
     print('{0} released'.format(
@@ -16,13 +14,9 @@ def on_release(key):
         return False
 
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener( on_press=on_press,on_release=on_release) as listener:
     listener.join()
 
 # ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
+listener = keyboard.Listener(on_press=on_press,on_release=on_release)
 listener.start()
