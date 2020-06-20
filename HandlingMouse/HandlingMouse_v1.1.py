f97a00f168d73d1e51957dc7609ad8a056b680a8
Trance <wuzheyuan.86@gmail.com>
2020-06-20 19:44:46 +0800

    2020.6.20

--- HandlingMouse/HandlingMouse_v1.1.py
+++ HandlingMouse/HandlingMouse_v1.1.py
@@ -1,6 +1,11 @@
-from pynput import keyboard
+` from pynput import keyboard
 from pynput.keyboard import Key, Listener   
 from pynput.mouse import Button, Controller
+
+up=False
+down=False
+left=False
+right=False
 
 mouse = Controller() 
 def on_press(key):
@@ -12,29 +17,18 @@ def on_press(key):
 def on_release(key):
     print('{0} have been released'.format(key))
     position=mouse.position
-    if  key == keyboard.Key.up:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(0,-10)
+    if  key != keyboard.Key.up:
+        up=True
     if key == keyboard.Key.down:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(0,10)
+        down=True
     if key == keyboard.Key.left:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(-10,0)
+        left=True
     if key == keyboard.Key.right:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
-        mouse.move(10,0)
+        right=True
     if key == keyboard.Key.f13:
-        print('********')
         mouse.press(Button.left)
         mouse.release(Button.left)
     if key == keyboard.Key.f15:
-        print('********')
-        # mouse.move(position[0]+10,position[1])
         mouse.press(Button.right)
         mouse.release(Button.right)
     if key == keyboard.Key.esc:
@@ -46,5 +40,13 @@ with keyboard.Listener( on_press=on_press,on_release=on_release) as listener:
     listener.join() 
     # if on_press           
 
-
+while True:
+    if up:
+        mouse.move(0,-10)
+    if down:
+        mouse.move(0,10)
+    if left:
+        mouse.move(-10,0)
+    if right:
+        mouse.move(10,0)
            
