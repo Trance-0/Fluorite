4cfb865a4f21b59af5f17ae12fc548039bc577b7
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2020-09-03 19:26:17 +0800

    2020.9.3

--- AudioMap/2Dmap.py
+++ AudioMap/2Dmap.py
@@ -10,14 +10,19 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 def convert_to_color(number):
-    result=[]
-    r=number//32//32
-    g=(number-r*32**2)//32
-    b=number%32
-    result.append(r*4)
-    result.append(g*4)
-    result.append(b*4)
-    return result
+    color=(((32747)/(number+32747)/2))*256
+    print (color)
+    return [color,color,color]
+
+# def convert_to_color(number):
+#     result=[]
+#     r=number//32//32
+#     g=(number-r*32**2)//32
+#     b=number%32
+#     result.append(r*4)
+#     result.append(g*4)
+#     result.append(b*4)
+#     return result
 
 CHUNK = 16384#我把它理解为缓冲流
 
@@ -37,8 +42,8 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 
 data = wf.readframes(CHUNK)#音频数据初始化
 status=True
-count=16#设置间隔区
-imageWidth=32766//count+1
+count=2#设置间隔区
+imageWidth=32767//count+1
 maxcount=0
 mincount=0
 img = np.zeros([1000,imageWidth,3])
@@ -57,7 +62,7 @@ while data != '' and status:#直到音频放完
 
     pitch=0
     for n in range(0,numpydata.size,count):#从频域中的2048个数据中没隔count个数据中选取一条
-        hight=abs(numpydata[n])
+        hight=numpydata[n]
         if hight>maxcount:
             maxcount=hight
         if hight<mincount:
@@ -68,12 +73,13 @@ while data != '' and status:#直到音频放完
         img[index,pitch,1]=color[1]
         img[index,pitch,2]=color[2]
         pitch+=1
-    # print (len(height))
-    # print("maxcount="+str(maxcount))
-    # print("mincount="+str(mincount))
+    # print (numpydata.size)
+    print("maxcount="+str(maxcount))
+    print("mincount="+str(mincount))
     index+=1
     cv.imshow("image", img)
     if cv.waitKey(1) == ord('q'):
+        cv.imwrite( "test_2.jpg", img );
         break
 
 stream.stop_stream()
