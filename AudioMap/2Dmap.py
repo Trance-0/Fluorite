4a62bb0bc71955d875e53bf95d78ba0387dc76eb
Trance <wuzheyuan.86@gmail.com>
2020-09-03 17:30:51 +0800

    2020.9.3

--- AudioMap/2Dmap.py
+++ AudioMap/2Dmap.py
@@ -9,6 +9,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+def convert_to_color(number):
+    result=[]
+    r=number//32//32
+    g=(number-r*32**2)//32
+    b=number%32
+    result.append(r*4)
+    result.append(g*4)
+    result.append(b*4)
+    return result
+
 CHUNK = 16384#我把它理解为缓冲流
 
 path = os.path.dirname(os.path.abspath(__file__))
@@ -27,7 +37,12 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 
 data = wf.readframes(CHUNK)#音频数据初始化
 status=True
-
+count=16#设置间隔区
+imageWidth=32766//count+1
+maxcount=0
+mincount=0
+img = np.zeros([1000,imageWidth,3])
+index=0
 while data != '' and status:#直到音频放完
     height=[]
     # elapsedtime=0
@@ -36,27 +51,30 @@ while data != '' and status:#直到音频放完
     data = wf.readframes(CHUNK)#更新data
     numpydata = np.fromstring(data, dtype=np.int16)#把data由字符串以十六进制的方式转变为数组
     # print(len(numpydata))
-    count=3#设置间隔区
     # for n in range(0,numpydata.size,count):#从频域中的2048个数据中没隔count个数据中选取一条
     #     hight=abs(int(numpydata[n]//1000))#对这么多数据取整和绝对值
     #     height.append(hight)
 
+    pitch=0
     for n in range(0,numpydata.size,count):#从频域中的2048个数据中没隔count个数据中选取一条
-        hight=abs(int(numpydata[n]//1000))#对这么多数据取整和绝对值
+        hight=abs(numpydata[n])
+        if hight>maxcount:
+            maxcount=hight
+        if hight<mincount:
+            mincount=hight
         height.append(hight)
-        # colors = plt.cm.viridis(hight / 10.)
-    print (len(height))
-
-    # print(height)
-    # ax3d.plot_trisurf(X, Y, Z,cmap="hsv")
-
-    theta = np.linspace(0.0, 2 * np.pi, len(height), endpoint=False)
-
-    width=2*np.pi/len(height)
-    # colors = plt.cm.viridis(height / 10.)
-    # print(width)
-    h=np.array(height)
-    
+        color=convert_to_color(hight)
+        img[index,pitch,0]=color[0]
+        img[index,pitch,1]=color[1]
+        img[index,pitch,2]=color[2]
+        pitch+=1
+    # print (len(height))
+    # print("maxcount="+str(maxcount))
+    # print("mincount="+str(mincount))
+    index+=1
+    cv.imshow("image", img)
+    if cv.waitKey(1) == ord('q'):
+        break
 
 stream.stop_stream()
 stream.close()
