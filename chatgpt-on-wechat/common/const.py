a124ba603f2ba5cf5e4b9b3548d43c787d5ed518
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2024-01-05 21:33:03 +0800

    gpt change

---
+++ chatgpt-on-wechat/common/const.py
@@ -0,0 +1,12 @@
+# bot_type
+OPEN_AI = "openAI"
+CHATGPT = "chatGPT"
+BAIDU = "baidu"
+XUNFEI = "xunfei"
+CHATGPTONAZURE = "chatGPTOnAzure"
+LINKAI = "linkai"
+
+VERSION = "1.3.0"
+
+CLAUDEAI = "claude"
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "xunfei","claude"]
